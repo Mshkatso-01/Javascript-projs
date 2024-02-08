145a01d811f7a7b2ed37e87630b26902ecdbadab
Mishkat Suliman <mshkatso.sul0@gmail.com>
2024-02-08 14:24:13 +0300

    first commit

---
+++ counter.js
@@ -0,0 +1,9 @@
+// document.getElementById(count-el).innerText = 5
+let bounsPoint = 50
+console.log(bounsPoint)
+bounsPoint = bounsPoint + 50
+console.log(bounsPoint)
+bounsPoint = bounsPoint - 75
+console.log(bounsPoint)
+bounsPoint = bounsPoint + 45
+console.log(bounsPoint)
